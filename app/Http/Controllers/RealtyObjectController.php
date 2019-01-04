273623473e07cbc03624c0a67efdd9fef1bdc374
Student-IS <correctmode@outlook.com>
2019-01-04 19:26:10 +0300

    Новые контроллеры

---
+++ app/Http/Controllers/RealtyObjectController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\RealtyObject;
+use Illuminate\Http\Request;
+
+class RealtyObjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\RealtyObject  $realtyObject
+     * @return \Illuminate\Http\Response
+     */
+    public function show(RealtyObject $realtyObject)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\RealtyObject  $realtyObject
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(RealtyObject $realtyObject)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\RealtyObject  $realtyObject
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, RealtyObject $realtyObject)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\RealtyObject  $realtyObject
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(RealtyObject $realtyObject)
+    {
+        //
+    }
+}
